8dcb51fbb43512e45603889df54e6499056547df
Joon <jwjung224@gmail.com>
2017-02-10 16:25:26 -0800

    Add files via upload

---
+++ styles/date.js
@@ -0,0 +1,34 @@
+﻿// JavaScript Date Function - edited from W3Schools tutorial by Lauribeth Hull
+
+//Changes the contents of an element with the id="date"
+function startTime()
+{
+var today=new Date();
+var h=today.getHours();
+var m=today.getMinutes();
+var s=today.getSeconds();
+
+m=checkTime(m);
+s=checkTime(s);
+document.getElementById('date').innerHTML=Date().toString().substring(0,15)+ ", " +h+":"+m+":"+s;
+t=setTimeout('startTime()',500);
+}
+
+
+
+// a function to add a zero in front of numbers<10
+function checkTime(i)
+{
+if (i<10)
+  {
+  i="0" + i;
+  }
+return i;
+}
+
+// function to 
+
+function practice()
+{
+	document.getElementById('abc').innerHTML = "This is Joon's Javascript Practice No.1.";
+}
